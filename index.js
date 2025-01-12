55bfa9e23eb573f581fdbc33bc280d96b2c3ed27
MagstarDev <yr6190457@gmail.com>
2025-01-12 20:44:55 +0500

    Skills-section V1

---
+++ index.js
@@ -0,0 +1,8 @@
+function showSidebar(){
+    const sidebar = document.querySelector('.side-bar')
+    sidebar.style.display = 'flex';
+}
+function hideSidebar(){
+    const sidebar = document.querySelector('.side-bar')
+    sidebar.style.display = 'none'
+}
