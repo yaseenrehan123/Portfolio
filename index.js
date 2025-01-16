cbf27a915192666ab8eed24fd2f4f297d83deede
MagstarDev <yr6190457@gmail.com>
2025-01-16 18:53:57 +0500

    Stable V1

--- index.js
+++ index.js
@@ -6,3 +6,14 @@ function hideSidebar(){
     const sidebar = document.querySelector('.side-bar')
     sidebar.style.display = 'none'
 }
+
+const observer = new IntersectionObserver(entries =>{
+  entries.forEach(entry => {
+      if(entry.isIntersecting){
+        entry.target.classList.add('show-effect')
+      }
+    
+  });
+});
+const hiddenElements = document.querySelectorAll('.hidden-effect')
+hiddenElements.forEach((el) => observer.observe(el));
